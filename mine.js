c10ccbbe65e5eb8f6c77b3a9f4a6e76f5639d194
mohamedym677 <mohamedym677@gmail.com>
2024-12-03 02:04:24 +0200

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,637 @@
+// let questions = [
+//     {
+//         numb: 1,
+//         question: " I am an boy.",
+//         answer: "A. I am a boy",
+//         options: [
+//             "A. I am a boy",
+//             "B. I am boy",
+//             "C. I am an boy",
+//             "D. I am boying"
+//         ]
+//     },
+//     {
+//         numb: 2,
+//         question: "They is flowers",
+//         answer: "B. They are flowers",
+//         options: [
+//             "A. They is flower",
+//             "B. They are flowers",
+//             "C. They are flower",
+//             "D. They is flowers"
+//         ]
+//     },
+//     {
+//         numb: 3,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 4,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 5,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 6,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 7,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 8,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 9,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 10,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 11,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 12,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 13,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 14,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 15,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 16,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+
+//     {
+//         numb: 17,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 18,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 19,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 20,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 21,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 22,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 23,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 24,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     },
+//     {
+//         numb: 25,
+//         question: "",
+//         answer: " ",
+//         options: [
+//             "A.  ",
+//             "B. ",
+//             "C. ",
+//             "D. "
+//         ]
+//     }
+// ];
+
+// // {
+// //         numb: 5,
+// //         question: "What does XML stand for?",
+// //         answer: "D. Extensible Markup Language",
+// //         options: [
+// //             "A. Excellent Multiple Language",
+// //             "B. Explore Multiple Language",
+// //             "C. Extra Markup Language",
+// //             "D. Extensible Markup Language"
+// //         ]
+// //     },
+
+
+
+
+
+
+
+
+
+
+
+let questions = [
+    {
+        numb: 1,
+        question: " I am an boy.",
+        answer: "A. I am a boy",
+        options: [
+            "A. I am a boy",
+            "B. I am boy",
+            "C. I am an boy",
+            "D. I am boying"
+        ]
+    },
+    {
+        numb: 2,
+        question: " They is flowers.",
+        answer: "B. They are flowers",
+        options: [
+            "A. They is flower",
+            "B. They are flowers",
+            "C. They are flower",
+            "D. They is flowers"
+        ]
+    },
+    {
+        numb: 3,
+        question: " They are bus driver.",
+        answer: "A. They are bus drivers",
+        options: [
+            "A. They are bus drivers",
+            "B. They are driver",
+            "C. They is bus driver",
+            "D. They is bus drivers"
+        ]
+    },
+    {
+        numb: 4,
+        question: " We is policemen.",
+        answer: "B. We are policemen",
+        options: [
+            "A. We are policeman",
+            "B. We are policemen",
+            "C. We is policeman",
+            "D. We is policemen"
+        ]
+    },
+    {
+        numb: 5,
+        question: " He is a postwoman.",
+        answer: "A. He is a postman",
+        options: [
+            "A. He is a postman",
+            "B. He are a postwoman",
+            "C. He is postwoman",
+            "D. He is postmans"
+        ]
+    },
+    {
+        numb: 6,
+        question: " Julie am twelve.",
+        answer: "B. Julie is twelve",
+        options: [
+            "A. Julie are twelve",
+            "B. Julie is twelve",
+            "C. Julie am twelve",
+            "D. Julie be twelve"
+        ]
+    },
+    {
+        numb: 7,
+        question: " They are potatos.",
+        answer: "A. They are potatoes",
+        options: [
+            "A. They are potatoes",
+            "B. They is potato",
+            "C. They are potato",
+            "D. They is potatos"
+        ]
+    },
+    {
+        numb: 8,
+        question: " They are babys.",
+        answer: "B. They are babies",
+        options: [
+            "A. They are baby",
+            "B. They are babies",
+            "C. They is babies",
+            "D. They are babys"
+        ]
+    },
+    {
+        numb: 9,
+        question: " John and Mary is teachers.",
+        answer: "B. John and Mary are teachers",
+        options: [
+            "A. John and Mary are teacher",
+            "B. John and Mary are teachers",
+            "C. John and Mary is teacher",
+            "D. John and Mary is teachers"
+        ]
+    },
+    {
+        numb: 10,
+        question: " The cat sleep on the table.",
+        answer: "A. The cat sleeps on the table",
+        options: [
+            "A. The cat sleeps on the table",
+            "B. The cat sleeping on the table",
+            "C. The cat sleep on table",
+            "D. The cats sleep on the table"
+        ]
+    },
+    {
+        numb: 11,
+        question: " What is the plural of 'baby'?",
+        answer: "C. Babies",
+        options: [
+            "A. Babys",
+            "B. Babyes",
+            "C. Babies",
+            "D. Babes"
+        ]
+    },
+    {
+        numb: 12,
+        question: " What is the plural of 'mouse'?",
+        answer: "A. Mice",
+        options: [
+            "A. Mice",
+            "B. Mouses",
+            "C. Mices",
+            "D. Mouse"
+        ]
+    },
+    {
+        numb: 13,
+        question: " What is the plural of 'tooth'?",
+        answer: "B. Teeth",
+        options: [
+            "A. Tooths",
+            "B. Teeth",
+            "C. Teeths",
+            "D. Toothes"
+        ]
+    },
+    {
+        numb: 14,
+        question: " What is the plural of 'goose'?",
+        answer: "D. Geese",
+        options: [
+            "A. Gooses",
+            "B. Goose",
+            "C. Geeses",
+            "D. Geese"
+        ]
+    },
+    {
+        numb: 15,
+        question: " What is the plural of 'person'?",
+        answer: "C. People",
+        options: [
+            "A. Persons",
+            "B. Persones",
+            "C. People",
+            "D. Peoples"
+        ]
+    },
+    {
+        numb: 16,
+        question: " Choose the correct spelling for the number 13:",
+        answer: "C. Thirteen",
+        options: [
+            "A. Thirtean",
+            "B. Thirtheen",
+            "C. Thirteen",
+            "D. Thertin"
+        ]
+    },
+    {
+        numb: 17,
+        question: " Choose the correct spelling for the number 40:",
+        answer: "B. Forty",
+        options: [
+            "A. Fourty",
+            "B. Forty",
+            "C. Fortee",
+            "D. Forte"
+        ]
+    },
+    {
+        numb: 18,
+        question: " Choose the correct spelling for the number 8:",
+        answer: "A. Eight",
+        options: [
+            "A. Eight",
+            "B. Aight",
+            "C. Eit",
+            "D. Eighht"
+        ]
+    },
+    {
+        numb: 19,
+        question: " Choose the correct spelling for the number 100:",
+        answer: "B. One Hundred",
+        options: [
+            "A. One Hundrid",
+            "B. One Hundred",
+            "C. On Hundred",
+            "D. One Hundread"
+        ]
+    },
+    {
+        numb: 20,
+        question: " Choose the correct spelling for the number 7:",
+        answer: "B. Seven",
+        options: [
+            "A. Seaven",
+            "B. Seven",
+            "C. Saven",
+            "D. Sevven"
+        ]
+    },
+    {
+        numb: 21,
+        question: " I ___ a teacher and you ___ a student.",
+        answer: "C. am, are",
+        options: [
+            "A. is, are",
+            "B. are, is",
+            "C. am, are",
+            "D. am, is"
+        ]
+    },
+    {
+        numb: 22,
+        question: " He ___ a baker and she ___ a chef.",
+        answer: "B. is, is",
+        options: [
+            "A. are, are",
+            "B. is, is",
+            "C. am, is",
+            "D. are, is"
+        ]
+    },
+    {
+        numb: 23,
+        question: " We ___ singers, and they ___ musicians.",
+        answer: "A. are, are",
+        options: [
+            "A. are, are",
+            "B. is, is",
+            "C. am, is",
+            "D. are, is"
+        ]
+    },
+    {
+        numb: 24,
+        question: " She ___ a nurse, and he ___ a doctor.",
+        answer: "C. is, is",
+        options: [
+            "A. am, is",
+            "B. are, is",
+            "C. is, is",
+            "D. is, are"
+        ]
+    },
+    {
+        numb: 25,
+        question: " You ___ a pilot, and I ___ a flight attendant.",
+        answer: "D. are, am",
+        options: [
+            "A. is, am",
+            "B. are, is",
+            "C. am, is",
+            "D. are, am"
+        ]
+    },
+    {
+        numb: 26,
+        question: " What is the number 23 in words?",
+        answer: "B. Twenty-three",
+        options: [
+            "A. One",
+            "B. Twenty-three",
+            "C. Thirty-three",
+            "D. Three"
+        ]
+    },
+    {
+        numb: 27,
+        question: " What is the number 5890 in words?",
+        answer: "B. Five thousand eight hundred ninety",
+        options: [
+            "A. Fifty-eight",
+            "B. Five thousand eight hundred ninety",
+            "C. Five thousand eighty",
+            "D. Five hundred eighty"
+        ]
+    },
+    {
+        numb: 28,
+        question: " What is the number 256732 in words?",
+        answer: "A. Two hundred fifty-six thousand seven hundred thirty-two",
+        options: [
+            "A. Two hundred fifty-six thousand seven hundred thirty-two",
+            "B. Two hundred fifty-six million seven hundred thirty-two",
+            "C. Two thousand five hundred sixty-seven hundred thirty-two",
+            "D. Two hundred five thousand six hundred thirty-two"
+        ]
+    },
+    {
+        numb: 29,
+        question: " What is the number 112478392 in words?",
+        answer: "A. One hundred twelve million four hundred seventy-eight thousand three hundred ninety-two",
+        options: [
+            "A. One hundred twelve million four hundred seventy-eight thousand three hundred ninety-two",
+            "B. One hundred twelve million four hundred seventy-eight thousand three hundred twenty-two",
+            "C. One hundred twelve million four thousand seven hundred thirty-two",
+            "D. One hundred twelve thousand four hundred seventy-eight thousand three hundred ninety-two"
+        ]
+    },
+    {
+        numb: 30,
+        question: " What is the number 179299292 in words?",
+        answer: "A. One hundred seventy-nine million two hundred ninety-nine thousand two hundred ninety-two",
+        options: [
+            "A. One hundred seventy-nine million two hundred ninety-nine thousand two hundred ninety-two",
+            "B. One hundred seventy-nine million two thousand two hundred ninety-two",
+            "C. One hundred seventy-nine billion two hundred ninety-nine thousand two hundred ninety-two",
+            "D. One hundred seventy-nine million two hundred thousand two hundred ninety-two"
+        ]
+    }
+];
+
+
+
+
